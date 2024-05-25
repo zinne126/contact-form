511133200006f3a07133dc67fb9b76e1897d3f74
zinne126 <109315861+zinne126@users.noreply.github.com>
2024-05-26 00:40:28 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,88 @@
 const form = document.getElementById("contact-form");
 const successMessage = document.getElementById("success-message");
-const firstName = document.getElementById("first-name");
-const lasttName = document.getElementById("last-name");
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-const consent = document.getElementById("consent");
 const sumbitBtn = document.getElementById("sumbit-btn");
 
-const queryType = document.querySelector('input[name="query-type"]:checked');
+form.addEventListener("click", (e) => {
+  e.preventDefault();
 
-const formAlert = document.querySelectorAll("form-alert");
-let isValid = true;
+  const firstName = document.getElementById("first-name").value.trim();
+  const lasttName = document.getElementById("last-name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const queryType = document.querySelector('input[name="query-type"]:checked');
+  const message = document.getElementById("message").value.trim();
+  const consent = document.getElementById("consent").checked;
 
-sumbitBtn.addEventListener("click", function (e) {
-  e.defaultPrevented;
-  console.log("hi");
+  const formAlert = document.querySelectorAll(".form-alert");
+
+  let isValid = true;
+
+  //first name validation
+  if (firstName === "") {
+    isValid = false;
+    document.querySelector("#first-name + .form-alert").style.display = "block";
+    // document.querySelector("#first-name").style.border = "1px solid var(--red)";
+  } else {
+    document.querySelector("#first-name + .form-alert").style.display = "none";
+    document.querySelector(".first-name").style.border =
+      "1px solid var(--medium-grey)";
+  }
+
+  //last name validation
+  if (lasttName === "") {
+    isValid = false;
+    document.querySelector("#last-name + .form-alert").style.display = "block";
+    // document.querySelector("#first-name").style.border = "1px solid var(--red)";
+  } else {
+    document.querySelector("#last-name + .form-alert").style.display = "none";
+    document.querySelector(".last-name").style.border =
+      "1px solid var(--medium-grey)";
+  }
+
+  //email validation
+
+  if (email === "") {
+    isValid = false;
+    document.querySelector("#email + .form-alert").style.display = "block";
+    // document.querySelector("#first-name").style.border = "1px solid var(--red)";
+  } else {
+    document.querySelector("#email + .form-alert").style.display = "none";
+    // document.querySelector(".email").style.border =
+    ("1px solid var(--medium-grey)");
+  }
+
+  //query validation
+  // if (!queryType) {
+  // isValid = false;
+  //document.querySelector(".radio-inputs" + ".form-alert").style.display =
+  //"block";
+  //} else {
+  //document.querySelector(".radio-inputs" + ".form-alert").style.display =
+  //"none";
+  //}
+
+  //message validation
+  if (message === "") {
+    isValid = false;
+    document.querySelector("#message + .form-alert").style.display = "block";
+    // document.querySelector("#message").style.border = "1px solid var(--red)";
+  } else {
+    document.querySelector("#message + .form-alert").style.display = "none";
+    //document.querySelector(".message").style.border =
+    ("1px solid var(--medium-grey)");
+  }
+
+  //consent validation
+  if (!consent) {
+    isValid = false;
+
+    formAlert[5].classList.add("form-error");
+  } else {
+    formAlert[5].classList.remove("form-error");
+  }
+
+  //form valid
+  if (isValid) {
+    successMessage.classList.add("active");
+    form.reset();
+  }
 });
